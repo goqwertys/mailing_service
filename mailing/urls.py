7dca29a40d11d6b4d969c415d189be88b8f29271
georgii <goqwertys@gmail.com>
2024-11-04 12:50:24 +0400

    models registered in admin.py

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-
-from config.urls import urlpatterns
 from mailing.apps import MailingConfig
+
 
 app_name =MailingConfig.name
 
 urlpatterns = [
-    path('', ),
-    path('', ),
-    path('', ),
 
 ]
