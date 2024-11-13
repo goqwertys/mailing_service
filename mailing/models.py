cef444045e097bd9db039c8a80609e2223abc879
georgii <goqwertys@gmail.com>
2024-11-14 02:53:51 +0400

    user column added to models

--- mailing/models.py
+++ mailing/models.py
@@ -14,7 +14,13 @@ class Recipient(models.Model):
     )
     name = models.CharField(max_length=255)
     comment = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipients')
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipients',
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return f'{self.email} {self.name}'
@@ -22,19 +28,19 @@ class Recipient(models.Model):
     class Meta:
         verbose_name = 'recipient'
         verbose_name_plural = 'recipients'
-        permissions = [
-            ('view_recipient', "Can view recipient"),
-            ('add_recipient', 'Can add recipient'),
-            ('change_recipient', 'Can change recipient'),
-            ('delete_recipient', 'Can delete recipient'),
-        ]
 
 
 class Message(models.Model):
     """ Message model """
     subject = models.CharField(max_length=255)
     body = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='messages',
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return f'{self.subject} {self.body[:20]}'
@@ -42,12 +48,6 @@ class Message(models.Model):
     class Meta:
         verbose_name = 'message'
         verbose_name_plural = 'messages'
-        permissions = [
-            ('view_message', 'Can view message'),
-            ('add_message', 'Can add message'),
-            ('change_message', 'Can change message'),
-            ('delete_message', 'Can delete message'),
-        ]
 
 
 class Mailing(models.Model):
@@ -66,8 +66,13 @@ class Mailing(models.Model):
         verbose_name='Mailing status'
     )
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='mailings')
-    recipient = models.ManyToManyField(Recipient)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='mailings')
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='mailings',
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return f'Mailing {self.id} - {self.status}'
@@ -76,10 +81,7 @@ class Mailing(models.Model):
         verbose_name = 'mailing'
         verbose_name_plural = 'mailings'
         permissions = [
-            ('view_mailing', 'Can view mailing'),
-            ('add_mailing', 'Can add mailing'),
-            ('change_mailing', 'Can change mailing'),
-            ('delete_mailing', 'Can delete mailing'),
+            ("disable_mailing", "Can disable mailing"),
         ]
 
 
@@ -107,6 +109,3 @@ class Attempt(models.Model):
     class Meta:
         verbose_name = 'attempt'
         verbose_name_plural = 'attempts'
-        permissions = [
-            ('view_attempt', 'Can view attempt'),
-        ]
